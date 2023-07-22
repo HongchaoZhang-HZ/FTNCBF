d8c4cea846a9ae1a6a8b571eae9a811a33683897
Hongchao Zhanng <hongchao@wustl.edu>
2023-07-22 12:14:47 -0500

    Comments added

--- Cases/Case.py
+++ Cases/Case.py
@@ -3,19 +3,57 @@ import torch
 import sympy as sp
 
 class case:
-    def __init__(self, DOMAIN, CTRLDOM, discrete=False):
+    '''
+    Define base class for different case studies
+    Input Basic information including
+        DOMAIN: State space
+        CTRLDOM: Control space
+        discrete: If the system is discrete-time (DT) then True
+                  otherwise False for continuous-time (CT)
+    Background:
+        The system is considered as CT/DT nonlinear control-affine system
+        with state 'x' whose dimension is determined by len(DOMAIN).
+        The dynamical model is defined as follows:
+            DT: x_nxt = f(x) + g(x) * u
+            CT: x_dot = f(x) + g(x) * u
+    '''
+    def __init__(self, DOMAIN: list, CTRLDOM: list, discrete=False):
+        '''
+        :param DOMAIN: [list] State space
+        :param CTRLDOM: [list] Control space
+        :param discrete: [bool] If the system is discrete-time then True
+        '''
         self.CTRLDOM = CTRLDOM
         self.DOMAIN = DOMAIN
         self.DIM = len(self.DOMAIN)
         self.discrete = discrete
 
     def f_x(self, x):
+        '''
+        Control affine model f(x)
+        :param x: [np.array/torch.Tensor] input state x in R^n
+        :return: [np.array/torch.Tensor] output in R^n
+        '''
         f_x = self.fx * x
         return f_x
 
     def g_x(self, x):
+        '''
+        Control affine model g(x)
+        :param x: [np.array/torch.Tensor] input state x in R^n
+        :return: [np.array/torch.Tensor] output in R^n
+        '''
         g_x = self.gx * x
         return g_x
+
     def xdot(self, x, u):
+        '''
+        The dynamical model is defined as follows:
+            DT: x_nxt = f(x) + g(x) * u
+            CT: x_dot = f(x) + g(x) * u
+        :param x: [np.array/torch.Tensor] input state x in R^n
+        :param u: [np.array/torch.Tensor] control input u in R^m
+        :return: [np.array/torch.Tensor] output in R^n
+        '''
         xdot = self.f_x(x) + self.g_x(x) @ u
         return xdot
--- Cases/Darboux.py
+++ Cases/Darboux.py
@@ -3,22 +3,46 @@ import torch
 from Cases.Case import *
 
 class Darboux(case):
+    '''
+    Define classical control case Darboux
+    '''
     def __init__(self):
+        '''
+        Define classical control case Darboux.
+        The system is 2D open-loop nonlinear CT system
+        '''
         DOMAIN = [[-2, 2], [-2, 2]]
         CTRLDOM = [-2, 2]
         discrete = False
         super().__init__(DOMAIN, CTRLDOM, discrete=discrete)
 
     def f_x(self, x):
+        '''
+        Control affine model f(x)
+        f0 = x0 + 2*x0*x1
+        f1 = -x0 + 2*x0^2 - x1^2
+        :param x: [np.array/torch.Tensor] input state x in R^n
+        :return: [np.array/torch.Tensor] output in R^n
+        '''
         x0_dot = x[:, 0] + 2 * x[:, 0] * x[:, 1]
         x1_dot = -x[:, 0] + 2 * x[:, 0] ** 2 - x[:, 1] ** 2
         x_dot = torch.vstack([x0_dot, x1_dot])
         return x_dot
 
     def g_x(self, x):
+        '''
+        Control affine model g(x)=[0 0 0]'
+        :param x: [np.array/torch.Tensor] input state x in R^n
+        :return: [np.array/torch.Tensor] output in R^n
+        '''
         gx = torch.zeros([self.DIM, 1])
         return gx
 
     def h_x(self, x):
+        '''
+        Define safe region C:={x|h_x(x) >= 0}
+        :param x: [np.array/torch.Tensor] input state x in R^n
+        :return: [np.array/torch.Tensor] scalar output in R
+        '''
         hx = (x[:, 0] + x[:, 1] ** 2)
         return hx
