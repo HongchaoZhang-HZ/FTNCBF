2e8b2f20ea008e85ae07e8c771c5cb52d2f06968
Hongchao Zhanng <hongchao@wustl.edu>
2023-07-22 14:42:43 -0500

    description

--- Cases/ObsAvoid.py
+++ Cases/ObsAvoid.py
@@ -3,6 +3,12 @@ import torch
 from Cases.Case import *
 
 class ObsAvoid(case):
+    '''
+    Define classical control case Obstacle Avoidance
+    x0_dot = v sin(phi) + 0
+    x1_dot = v cos(phi) + 0
+    phi_dot = 0         + u
+    '''
     def __init__(self):
         DOMAIN = [[-2, 2], [-2, 2], [-2, 2]]
         CTRLDOM = [-2, 2]
@@ -11,9 +17,15 @@ class ObsAvoid(case):
         super().__init__(DOMAIN, CTRLDOM, discrete=discrete)
 
     def f_x(self, x):
-        # x0_dot = v sin(phi)
-        # x1_dot = v cos(phi)
-        # phi_dot = 0
+        '''
+        Control affine model f(x)
+        f0 = v sin(phi)
+        f1 = v cos(phi)
+        f2 = 0
+        :param x: [np.array/torch.Tensor] input state x in R^n
+        :return: [np.array/torch.Tensor] output in R^n
+        '''
+
         v = self.v
         x0_dot = v * torch.sin(x[:, 2])
         x1_dot = v * torch.cos(x[:, 2])
@@ -22,7 +34,11 @@ class ObsAvoid(case):
         return x_dot
 
     def g_x(self, x):
-        # gx = [0, 0, 1]'
+        '''
+        Control affine model g(x)=[0 0 1]'
+        :param x: [np.array/torch.Tensor] input state x in R^n
+        :return: [np.array/torch.Tensor] output in R^n
+        '''
         g_x0 = torch.zeros(len(x))
         g_x1 = torch.zeros(len(x))
         g_phi = torch.ones(len(x))
@@ -30,5 +46,11 @@ class ObsAvoid(case):
         return gx
 
     def h_x(self, x):
+        '''
+        Define safe region C:={x|h_x(x) >= 0}
+        The safe region is a pole centered at (0,0,any) with radius 0.2
+        :param x: [np.array/torch.Tensor] input state x in R^n
+        :return: [np.array/torch.Tensor] scalar output in R
+        '''
         hx = -(x[:, 0]**2 + x[:, 1] ** 2) + 0.04
         return hx
