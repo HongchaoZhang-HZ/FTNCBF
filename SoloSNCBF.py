cf2d92a8bd774e2993ebda625e72084a8a7b1965
Hongchao Zhanng <hongchao@wustl.edu>
2023-08-20 11:32:06 -0500

    solo sncbf training

--- SoloSNCBF.py
+++ SoloSNCBF.py
@@ -42,7 +42,8 @@ class SNCBF_Synth(NCBF_Synth):
         super().__init__(arch, act_layer, case, verbose=False)
         # Under construction: Critic is designed to tuning loss fcn automatically
         # self.critic = NeuralCritic(case)
-        lctime = time.ctime(time.time())
+        # lctime = time.ctime(time.time())
+        lctime = time.strftime("%Y%m%d%H%M%S")
         # Tensorboard
         self.writer = SummaryWriter(f'./runs/SNCBF/{lctime}'.format(lctime))
 
@@ -84,9 +85,9 @@ class SNCBF_Synth(NCBF_Synth):
     def get_model(self):
         return self.model
 
-    def get_grad(self, x):
-        grad_input = torch.tensor(x, requires_grad=True, dtype=torch.float)
-        return torch.autograd.grad(self.model.forward(grad_input), grad_input)
+    # def get_grad(self, x):
+    #     grad_input = torch.tensor(x, requires_grad=True, dtype=torch.float)
+    #     return torch.autograd.grad(self.model.forward(grad_input), grad_input)
 
     def get_hessian(self, x):
         grad_input = torch.tensor(x, requires_grad=True, dtype=torch.float)
@@ -153,11 +154,13 @@ class SNCBF_Synth(NCBF_Synth):
         #           @ self.case.f_x(X_batch).transpose(0, 1).unsqueeze(2)).squeeze()
         # dbdxgx = (grad_vector.transpose(0, 1).unsqueeze(1)
         #           @ self.case.g_x(X_batch).transpose(0, 1).unsqueeze(2)).squeeze()
-        dbdxfx = (grad_vector.unsqueeze(-1).transpose(-1,-2)
+        # compute loss based on (db/dx)*fx + (db/dx)*gx*u
+        dbdxfx = (grad_vector.unsqueeze(-1).transpose(-1, -2)
                   @ self.case.f_x(X_batch).transpose(0, 1).unsqueeze(2)).squeeze()
-        dbdxgx = (grad_vector.unsqueeze(-1).transpose(-1,-2)
+        dbdxgx = (grad_vector.unsqueeze(-1).transpose(-1, -2)
                   @ self.case.g_x(X_batch).transpose(0, 1).unsqueeze(2)).squeeze()
         u = self.feasible_u(dbdxfx, dbdxgx)
+        # u = -X_batch[:,-1]
         # update delta_gamma
         self.delta_gamma = self.numerical_delta_gamma(grad_vector, gamma)
         # EKF_term = grad_vector.transpose(0,1) @ self.ekf_gain @ self.c
@@ -186,11 +189,14 @@ class SNCBF_Synth(NCBF_Synth):
         return torch.max(violations, torch.zeros([1, batch_length]))
 
     def train(self, num_epoch, num_restart=10, alpha1=None, alpha2=None, warm_start=False):
-        optimizer = optim.Adam(self.model.parameters(), lr=1e-8)
+        if warm_start:
+            learning_rate = 1e-2
+        else:
+            learning_rate = 1e-2
+        optimizer = optim.Adam(self.model.parameters(), lr=learning_rate, weight_decay=0.01)
         scheduler = ExponentialLR(optimizer, gamma=0.99)
         # define hyper-parameters
-        if alpha1 is None and alpha2 is None:
-            alpha1, alpha2 = 1, 0
+        alphaf, alpha1, alpha2 = 1, 1, 0.0001
         # 1, 1e-8
         # Set alpha2=0 for feasibility test with Floss quickly converge to 0
         # If set alpha2 converges but does not pass the verification, then increase the sampling number.
@@ -198,7 +204,9 @@ class SNCBF_Synth(NCBF_Synth):
         rlambda = 1
 
         # Generate data
-        size = 80
+        size = 128
+        volume = torch.Tensor([0])
+        volume_pre = torch.Tensor([0])
         rdm_input = self.generate_data(size)
         # rdm_input = self.generate_input(shape)
         # ref_output = torch.unsqueeze(self.h_x(rdm_input.transpose(0, self.DIM)), self.DIM)
@@ -222,42 +230,57 @@ class SNCBF_Synth(NCBF_Synth):
                     model_output = self.forward(X_batch)
 
                     warm_start_loss = self.warm_start(y_batch, model_output)
-                    correctness_loss = self.safe_correctness(y_batch, model_output, l_co=1, alpha1=alpha1, alpha2=alpha2)
+                    correctness_loss, coverage_loss = self.safe_correctness(y_batch, model_output, l_co=1,
+                                                                            alpha1=alpha1, alpha2=alpha2)
                     # trivial_loss = self.trivial_panelty(ref_output, self.model.forward(rdm_input), 1)
                     trivial_loss = self.trivial_panelty(y_batch, model_output, 1)
 
                     # grad = self.numerical_gradient(X_batch, model_output, batch_length, epsilon=0.001)
                     # grad_vector = torch.vstack(grad)
                     feasibility_output = 0
+                    ce_indicator = 0
                     grad_vector = torch.vstack([self.get_grad(x)[0] for x in X_batch])
                     for i in range(len(self.FTEKF_gain_list)):
-                        feasibility_output += self.feasibility_loss(grad_vector, X_batch,
-                                                                    self.FTEKF_gain_list[i], self.gamma[i])
+                        tempt_feas_loss = self.feasibility_loss(grad_vector, X_batch,
+                                                                self.FTEKF_gain_list[i], self.gamma[i])
+                        feasibility_output += batch_length * (torch.relu(model_output.squeeze()) *
+                                                              torch.relu(-model_output.squeeze()+1e-2) *
+                                                              (tempt_feas_loss + model_output.squeeze()))
+                        ce_indicator += (torch.relu(model_output.squeeze())/model_output.squeeze() *
+                                        torch.relu(-model_output.squeeze()+1e-2)/(-model_output.squeeze()+1e-2) *
+                                        torch.relu(-tempt_feas_loss - model_output.squeeze())
+                                        /((-tempt_feas_loss - model_output.squeeze())))
                     # check_item = torch.max((-torch.abs(model_output)+0.2).reshape([1, batch_length]), torch.zeros([1, batch_length]))
                     # feasibility_loss = torch.sum(torch.tanh(check_item*feasibility_output))
 
                     # Our loss function
                     # violations = -check_item * self.feasible_violations(model_output, feasibility_output, batch_length, rlambda)
                     # Chuchu Fan loss function
-                    violations = -1 * self.feasible_violations(model_output, feasibility_output, batch_length, rlambda)
+                    # violations = -1 * self.feasible_violations(model_output, feasibility_output, batch_length, rlambda)
                     # violations = -1 * feasibility_output - torch.max(rlambda * torch.abs(model_output.transpose(0, 1)),
                     #                                                  torch.zeros([1, batch_length]))
-                    feasibility_loss = 2 * torch.sum(torch.max(violations - 1e-4, torch.zeros([1, batch_length])))
+                    # feasibility_loss = 2 * torch.sum(torch.max(violations - 1e-4, torch.zeros([1, batch_length])))
+                    feasibility_loss = torch.relu(-feasibility_output).sum()
                     mseloss = torch.nn.MSELoss()
                     # loss = self.def_loss(1 * correctness_loss + 1 * feasibility_loss + 1 * trivial_loss)
-                    floss = mseloss(torch.max(violations - 1e-4, torch.zeros([1, batch_length])), torch.zeros(batch_length))
-                    tloss = mseloss(trivial_loss, torch.Tensor([0.0]))
+                    # floss = mseloss(torch.max(violations - 1e-4, torch.zeros([1, batch_length])), torch.zeros(batch_length))
+                    # tloss = mseloss(trivial_loss, torch.Tensor([0.0]))
                     if warm_start:
-                        loss = self.warm_start(y_batch, model_output)
+                        correctness_loss, coverage_loss = self.safe_correctness(y_batch, model_output, l_co=1, alpha1=1,
+                                                                                alpha2=0.0001)
+                        loss = correctness_loss + coverage_loss + trivial_loss
                     else:
-                        loss = correctness_loss + feasibility_loss + tloss
-
+                        # loss = feasibility_loss
+                        loss = (alpha1 * correctness_loss + alpha2 * coverage_loss
+                                + alphaf * feasibility_loss + trivial_loss)
 
                     loss.backward()
                     # with torch.no_grad():
                     #     loss = torch.max(loss)
                     optimizer.step()
                     optimizer.zero_grad()
+                    alpha1 += 0.1 * correctness_loss.item()
+                    # alphaf += 0.1 * feasibility_loss.item()
 
                     # Print Detailed Loss
                     running_loss += loss.item()
@@ -271,24 +294,30 @@ class SNCBF_Synth(NCBF_Synth):
                     #     alpha2 = 0
 
                 # Log details of losses
-                if not warm_start:
-                    self.writer.add_scalar('Loss/Loss', running_loss, self.run*num_epoch+epoch)
-                    self.writer.add_scalar('Loss/FLoss', feasibility_running_loss.item(), self.run*num_epoch+epoch)
-                    self.writer.add_scalar('Loss/CLoss', correctness_running_loss.item(), self.run*num_epoch+epoch)
-                    self.writer.add_scalar('Loss/TLoss', trivial_running_loss.item(), self.run*num_epoch+epoch)
-                # Log volume of safe region
+                # if not warm_start:
                 volume = self.compute_volume(rdm_input)
-                self.writer.add_scalar('Volume', volume, self.run*num_epoch+epoch)
-                # self.writer.add_scalar('Verifiable', veri_result, self.run * num_epoch + epoch)
-                # Process Bar Print Losses
+                self.writer.add_scalar('Loss/Loss', running_loss, self.run * num_epoch + epoch)
+                self.writer.add_scalar('Loss/FLoss', feasibility_running_loss.item(), self.run * num_epoch + epoch)
+                self.writer.add_scalar('Loss/CLoss', correctness_running_loss.item(), self.run * num_epoch + epoch)
+                self.writer.add_scalar('Loss/TLoss', trivial_running_loss.item(), self.run * num_epoch + epoch)
                 pbar.set_postfix({'Loss': running_loss,
                                   'Floss': feasibility_running_loss.item(),
                                   'Closs': correctness_running_loss.item(),
                                   'Tloss': trivial_running_loss.item(),
-                                  'PVeri': str(veri_result),
+                                  'WarmUp': str(warm_start),
+                                  'FCEnum': ce_indicator.sum().item(),
                                   'Vol': volume.item()})
                 pbar.update(1)
                 scheduler.step()
+                # Log volume of safe region
+
+                # self.writer.add_scalar('Volume', volume, self.run * num_epoch + epoch)
+                # scheduler.step()
+                if correctness_running_loss.item() < 1 and trivial_running_loss.item() < 0.001 and ce_indicator.sum().item() == 0 and volume > 0.2:
+                    if volume_pre < volume:
+                        torch.save(self.model.state_dict(),
+                                   f'Trained_model/NCBF/SNCBF_SoloGood{self.run}.pt'.format(self.run))
+                        volume_pre = volume
                 # if feasibility_running_loss <= 0.0001 and not warm_start:
                 #     try:
                 #         veri_result, num = self.veri.proceed_verification()
@@ -304,7 +333,7 @@ class SNCBF_Synth(NCBF_Synth):
             #         pass
             # if veri_result:
             #     torch.save(self.model.state_dict(), f'Trained_model/NCBF/NCBF_Obs{epoch}.pt'.format(epoch))
-            torch.save(self.model.state_dict(), f'Trained_model/SNCBF/SNCBF_Obs{self.run}.pt'.format(self.run))
+            # torch.save(self.model.state_dict(), f'Trained_model/SNCBF/SNCBF_Obs{self.run}.pt'.format(self.run))
 
 ObsAvoid = ObsAvoid()
 sensor_list = SensorSet([0, 1, 1, 2, 2], [0.001, 0.002, 0.0015, 0.001, 0.01])
